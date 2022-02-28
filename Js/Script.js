4445c3dfffa43dd4123372f6f2a20bfd78d70995
Shadman-Shakib-27 <S.S.A.27.8.1999@gmail.com>
2022-03-01 02:21:43 +0600

    My Second Commit.

--- Js/Script.js
+++ Js/Script.js
@@ -1,9 +1,8 @@
 const allPhone = () => {
   const searchInput = document.getElementById("search-input").value;
   searchInput.value = "";
-  console.log(searchInput);
-  const url = `https://openapi.programming-hero.com/api/phones?s=${searchInput}`;
 
+  const url = `https://openapi.programming-hero.com/api/phones?s=${searchInput}`;
   fetch(url)
     .then((res) => res.json())
     .then((datas) => displayPhone(datas.data));
@@ -11,11 +10,12 @@ const allPhone = () => {
 
 const displayPhone = (phones) => {
   const searchResults = document.getElementById("search-results");
+
   for (let phone of phones) {
     const div = document.createElement("div");
     div.classList.add("col");
-    div.innerHTML = `<div class="card h-100  border-info shadow mt-3">
-      <img src="${phone.image}" class="card-img-top w-25 mt-3 mx-auto" alt="..." />
+    div.innerHTML = `<div class="card h-100 border-info shadow mt-3">
+      <img src="${phone.image}" class="card-img-top w-25 mt-3        mx-auto" alt="..." />
          <div class="card-body">
           <h5 class="card-title text-center">${phone.phone_name}</h5>
           <h6 class="card-text text-center">
